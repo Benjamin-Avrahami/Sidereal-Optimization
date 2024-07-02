e08d57692e31cf2898fc2da749072819d7e60420
ACE3R2 <bavrahamics@gmail.com>
2024-07-02 15:17:01 -0400

    Wrapped resource for easy creation and created collection class

--- v2/resource.py
+++ v2/resource.py
@@ -4,34 +4,36 @@ class Resource(ABC):
     def __init__(self, resource_name):
         self.name = resource_name
     
+    # Get the name of the resource
     def get_name():
         return self.name
-        
+    
+    # See whether the other resource provided is the same resource as this
     def same_type(other_resource):
         return self.name == other_resource.name
         
         
         
 class Scorable(Resource):
+    # Suggested trade value (anchored to 1 victory point = 1)
     def tradeValue():
         pass
         
+    # Victory point total of the resource at the end of the game
     def scoringValue():
-        if self.name == 'VP':
+        if self.name == 'victory point':
             return tradeValue()
         else:
             return tradeValue()/2
-        
-        
-class Cube(Scorable):
+
     def isWild():
         pass
-
+        
 
 
 
 # brown, white, green, small grey
-class SmallCube(Cube):
+class SmallCube(Scorable):
     def tradeValue():
         return 1/3
         
@@ -40,7 +42,7 @@ class SmallCube(Cube):
         
 
 # yellow, black, blue, large grey
-class LargeCube(Cube):
+class LargeCube(Scorable):
     def tradeValue():
         return 1/2
         
@@ -48,12 +50,15 @@ class LargeCube(Cube):
         return self.name == 'large grey'
         
 
-# ship, octagon, VP
+# ship, octagon, victory point
 class OtherScorable(Scorable):
     def tradeValue():
         if self.name == 'ship':
             return 1/3
-        elif self.name == 'octagon' or self.name == 'VP':
+        elif self.name == 'octagon' or self.name == 'victory point':
             return 1
         else:
             return 0
+    
+    def isWild():
+        return False
