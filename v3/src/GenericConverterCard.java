952d604206c591a7816a8d22e5aac47777fa7b22
ACE3R2 <bavrahamics@gmail.com>
2024-09-18 05:00:46 -0400

    Adjusted the converter card to use state design pattern

--- v3/src/GenericConverterCard.java
+++ v3/src/GenericConverterCard.java
@@ -1,77 +1,45 @@
 import java.util.*;
 
 public class GenericConverterCard extends ConverterCard {
-
-	private String unupgradedConverterName;
-	private String upgradedConverterName;
+	
 	private boolean inPlay;
-	private ArrayList<ArrayList<ConverterFeatures>> cardConverters; // 1st set of converters is unupgraded, 2nd is upgraded
 	private boolean upgraded;
-	private ArrayList<ConverterFeatures> entryResources; // costs and benefits to putting the card in play
-	private ArrayList<ConverterFeatures> upgradeResources; // costs and benefits to upgrading the card
+	
+	private ConverterSide frontSide;
+	private ConverterSide backSide;
+	
+	// costs for entry/upgrading are not actual sides of the converter, but have similar behavior of running one out of several converters
+	private ConverterSide entrySide; // costs and benefits to putting the card in play
+	private ConverterSide upgradeSide; // costs and benefits to upgrading the card
 	
 	
-	// Assumes 1 converter on front and back side run during economy, no cost to enter play, (up to) 2 upgrades run during trade
-	public GenericConverterCard(String unupgradedName, Converter unupgradedConverter, String upgradedName, Converter upgradedConverter, ConverterCard upgrade1Cost, ConverterCard upgrade2Cost) {
-		ResourceCollection card1Cost = new BasicResourceCollection();
-		ResourceCollection card2Cost = new BasicResourceCollection();
-		card1Cost.add(upgrade1Cost);
-		card2Cost.add(upgrade2Cost);
-		Converter upgrade1Transaction = new Converter(card1Cost, new BasicResourceCollection());
-		Converter upgrade2Transaction = new Converter(card2Cost, new BasicResourceCollection());
-		ArrayList<Converter> upgradeCosts = new ArrayList<Converter>();
-		upgradeCosts.add(upgrade1Transaction);
-		upgradeCosts.add(upgrade2Transaction);
-		ArrayList<Converter> frontRun = new ArrayList<Converter>();
-		ArrayList<Converter> backRun = new ArrayList<Converter>();
-		frontRun.add(unupgradedConverter);
-		backRun.add(upgradedConverter);
-		constructFeatures(unupgradedName, upgradedName, frontRun, backRun, new ArrayList<Converter>(), upgradeCosts, false);
-	}
 	
 
-	public GenericConverterCard(String unupgradedConvName, String upgradedConvName, ArrayList<Converter> unupgradedConverters, ArrayList<Converter> upgradedConverters, ArrayList<Converter> entries, ArrayList<Converter> upgrades, boolean tradeRun) {
-		constructFeatures(unupgradedConvName, upgradedConvName, unupgradedConverters, upgradedConverters, entries, upgrades, tradeRun);
-	}
-	
-	public void constructFeatures(String unupgradedConvName, String upgradedConvName, ArrayList<Converter> unupgradedConverters, ArrayList<Converter> upgradedConverters, ArrayList<Converter> entries, ArrayList<Converter> upgrades, boolean tradeRun) {
-		ArrayList<ConverterFeatures> firstConverters = arrayConverterToFeatures(unupgradedConverters, unupgradedConvName, tradeRun);
-		ArrayList<ConverterFeatures> secondConverters = arrayConverterToFeatures(upgradedConverters, upgradedConvName, tradeRun); //Assumes both sides are run at the same time
-		cardConverters = new ArrayList<ArrayList<ConverterFeatures>>();
-		cardConverters.add(firstConverters);
-		cardConverters.add(secondConverters);
-		entryResources = arrayConverterToFeatures(entries, unupgradedConvName + "_entry", true); // Assumes entry during trade
-		upgradeResources = arrayConverterToFeatures(upgrades, unupgradedConvName + "_upgrade", true); // Assumes upgrade during trade
-		inPlay = entryResources.isEmpty();
-		unupgradedConverterName = unupgradedConvName;
-		upgradedConverterName = upgradedConvName;
+	public GenericConverterCard(ConverterSide side1, ConverterSide side2, ArrayList<Converter> entries, ArrayList<Converter> upgrades) {
+		frontSide = side1;
+		backSide = side2;
+		entrySide = new ConverterSide(side1.getName() + "_entry", entries, true); // Assumes entry during trade
+		upgradeSide = arrayConverterToFeatures(side1.getName() + "_upgrade", upgrades, true); // Assumes upgrade during trade
+		inPlay = entrySide.numConverterOptions.size() == 0;
 		upgraded = false;
 	}
-
-	private ArrayList<ConverterFeatures> arrayConverterToFeatures(ArrayList<Converter> convs, String convName, boolean instRun) {
-		ArrayList<ConverterFeatures> features = new ArrayList<ConverterFeatures>();
-		for (int i = 0; i < convs.size(); ++i) {
-			String currConvName = convName;
-			if (convs.size() > 1) {
-				currConvName = currConvName + "_" + i;
-			}
-			features.add(new ConverterFeatures(convs.get(i), currConvName, instRun));
-		}
-		return features;
+	
+	
+	// Returns current side that the card is on
+	// Will still return front side if card is not in play
+	private ConverterSide currentSide() {
+		if (!isUpgraded) return frontSide;
+		else return backSide;
 	}
 
 
 	// Get the name of the resource
 	public String getName() {
-		if (isUpgraded()) {
-			return upgradedConverterName;
-		}
-		else {
-			return unupgradedConverterName;
-		}
+		return currentSide().getName();
 	}
 
 	// See whether the other resource provided is the same resource as this
+	// If on opposite sides of the same card, will return unequal
 	public boolean equals(Object otherResource) {
 		return otherResource instanceof GenericConverterCard && this.getName().equals(((GenericConverterCard)otherResource).getName());
 	}
@@ -89,80 +57,79 @@ public class GenericConverterCard extends ConverterCard {
 	public boolean isInPlay() {
 		return inPlay;
 	}
-
-	private boolean runConverterFeatureFromArray(ArrayList<ConverterFeatures> convarry, int index, ResourceCollection startingResources, ResourceCollection futureResources) {
-		if (index >= convarry.size()) return false;
-		return convarry.get(index).runConverter(startingResources, futureResources);
+	
+	// Returns the number of converters available on the card
+	// If not in play, returns 0
+	public int numConverterOptions() {
+		if (!isInPlay()) return 0;
+		return currentSide().numConverterOptions();
 	}
 
+
 	// Runs the first legally available (not taking into account resources) converter on the card
+	// Returns whether running the converter was successful
 	public boolean run(ResourceCollection initialResources, ResourceCollection futureResources) {
 		return run(0, initialResources, futureResources);
 	}
 
+	// Runs the indexth available converter (0-indexed)
+	// Returns whether running the converter was successful
 	public boolean run(int index, ResourceCollection initialResources, ResourceCollection futureResources) {
 		if (!isInPlay()) return false;
-		if (isUpgraded()) {
-			return runConverterFeatureFromArray(cardConverters.get(1), index, initialResources, futureResources);
-		}
-		else {
-			return runConverterFeatureFromArray(cardConverters.get(0), index, initialResources, futureResources);
-		}
+		return currentSide().run(index, initialResources, futureResources);
 	}
 
+	// Attempts to enter play using the first method available (if there are several)
+	// Returns whether it was successful
 	public boolean enterPlay(ResourceCollection initialResources, ResourceCollection futureResources) {
 		return enterPlay(0, initialResources, futureResources);
 	}
 
+	// Attempts to enter play using the indexth method (0-indexed)
+	// Returns whether it was successful
 	public boolean enterPlay(int index, ResourceCollection initialResources, ResourceCollection futureResources) {
 		if (isInPlay()) return false;
-		return runConverterFeatureFromArray(entryResources, index, initialResources, futureResources);
+		inPlay = entrySide.run(index, initialResources, futureResources);
+		return inPlay;
 	}
 
+	// Attempts to upgrade the converter using the first method available (if there are several)
+	// Returns whether it was successful
 	public boolean upgrade(ResourceCollection initialResources, ResourceCollection futureResources) {
 		return upgrade(0, initialResources, futureResources);
 	}
 
+	// Attempts to upgrade the converter using the indexth method (0-indexed)
+	// Returns whether it was successful
 	public boolean upgrade(int index, ResourceCollection initialResources, ResourceCollection futureResources) {
 		if (!isInPlay()) return false;
 		if (isUpgraded()) return false;
-		return runConverterFeatureFromArray(upgradeResources, index, initialResources, futureResources);
+		upgraded = upgradeSide.run(index, initialResources, futureResources);
+		return upgraded;
 	}
 	
-	
+	// Prints information about the converter
 	public void display() {
 		System.out.println("Entry costs:");
-		for (int i = 0; i < entryResources.size(); ++i) {
-			System.out.println(i+1);
-			entryResources.get(i).display();
-		}
+		entrySide.display();
 		if (inPlay) {
-			System.out.print("This card is in play. ");
+			System.out.print("This card is in play ");
 			if (!upgraded) {
-				System.out.println("On Side 1.");
+				System.out.println("on Side 1.");
 			}
 			else {
-				System.out.println("On Side 2.");
+				System.out.println("on Side 2.");
 			}
 		}
 		else {
 			System.out.println("This card is not in play.");
 		}
-		System.out.println("Side 1 - " + unupgradedConverterName);
-		for (int i = 0; i < cardConverters.get(0).size(); ++i) {
-			System.out.println(i+1);
-			cardConverters.get(0).get(i).display();
-		}
+		System.out.println("Side 1 - ");
+		frontSide.display();
 		System.out.println("Upgrade costs:");
-		for (int i = 0; i < upgradeResources.size(); ++i) {
-			System.out.println(i+1);
-			upgradeResources.get(i).display();
-		}
-		System.out.println("Side 2 - " + upgradedConverterName);
-		for (int i = 0; i < cardConverters.get(1).size(); ++i) {
-			System.out.println(i+1);
-			cardConverters.get(1).get(i).display();
-		}
+		upgradeSide.display();
+		System.out.println("Side 2 - ");
+		backSide.display();
 		System.out.println("--------- End Card ----------");
 	}
 
